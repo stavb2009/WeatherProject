f3a04a3821e04bafc6f3039b7de0172585a8db6e
mryuv1 <mryuv1@gmail.com>
2021-12-17 22:48:59 +0200

    I fixed the DataLoader Class, It's better to code review it. Yuval - 17/12/21

--- dataLoader.py
+++ dataLoader.py
@@ -18,15 +18,12 @@ class WindowSlider(object):
         ====================
         w: window_size - number of time steps to look back
         o: offset between last reading and temperature
-        r: response_size - number of time steps to predict
-        l: maximum length to slide - (#observation - w)
-        p: final predictors - (#predictors * w)
+        s: response_size - number of time steps to predict
+        l: length to slide - ((#observation - w)/s)+1
         '''
         self.w = window_size
-        self.o = 0
-        self.r = 1
+        self.s = 1
         self.l = 0
-        self.p = 0
         self.names = []
 
     def re_init(self, arr):
@@ -36,60 +33,58 @@ class WindowSlider(object):
         arr = np.cumsum(arr)
         return arr - arr[0]
 
-    def collect_windows(self, X, window_size=5, offset=0, previous_y=False):
+    def collect_windows(self, X, window_size=5, step=1):
         '''
         Input: X is the input matrix, each column is a variable
         Returns: diferent mappings window-output
         '''
-        cols = len(list(X)) - 1
+        cols = len(list(X))
         N = len(X)
 
-        self.o = offset
         self.w = window_size
-        self.l = N - (self.w + self.r) + 1
-        if not previous_y: self.p = cols * (self.w)
-        if previous_y: self.p = (cols + 1) * (self.w)
+        self.s = step
+        self.l = int(np.floor((N -self.w)/self.s) + 1)
+
+        if self.l<0:
+            print("Class: WindowSlider, def: collect_windows, error: Length od input data is too short for the window")
+            return -1
 
         # Create the names of the variables in the window
         # Check first if we need to create that for the response itself
-        if previous_y: x = cp.deepcopy(X)
-        if not previous_y: x = X.drop(X.columns[-1], axis=1)
 
-        for j, col in enumerate(list(x)):
+        self.names.append(X.index.name)
+
+        for j, col in enumerate(list(X)):
 
             for i in range(self.w):
                 name = col + ('(%d)' % (i + 1))
                 self.names.append(name)
+        #
+        # # Incorporate the timestamps where we want to predict
+        # for k in range(self.s):
+        #     name = '∆t' + ('(%d)' % (self.w + k + 1))
+        #     self.names.append(name)
 
-        # Incorporate the timestamps where we want to predict
-        for k in range(self.r):
-            name = '∆t' + ('(%d)' % (self.w + k + 1))
-            self.names.append(name)
 
-        self.names.append('Y')
-
-        df = pd.DataFrame(np.zeros(shape=(self.l, (self.p + self.r + 1))),
+        df = pd.DataFrame(np.zeros(shape=(self.l,len(self.names))),
                           columns=self.names)
 
         # Populate by rows in the new dataframe
         for i in range(self.l):
 
-            slices = np.array([])
+            slices = np.array([X.index[i*self.w]])
 
             # Flatten the lags of predictors
-            for p in range(x.shape[1]):
+            for p in range(X.shape[1]):
 
-                line = X.values[i:self.w + i, p]
-                # Reinitialization at every window for ∆T
-                if p == 0: line = self.re_init(line)
+                line = X.values[i*self.s:self.w + i*self.s, p]
+
+
 
                 # Concatenate the lines in one slice
                 slices = np.concatenate((slices, line))
 
                 # Incorporate the timestamps where we want to predict
-            line = np.array([self.re_init(X.values[i:i + self.w + self.r, 0])[-1]])
-            y = np.array(X.values[self.w + i + self.r - 1, -1]).reshape(1, )
-            slices = np.concatenate((slices, line, y))
 
             # Incorporate the slice to the cake (df)
             df.iloc[i, :] = slices
@@ -140,42 +135,16 @@ deltaT = np.concatenate((np.array([0]), deltaT))
 df3.insert(2, '∆T', deltaT)
 
 
-data_set = df3["2019-12-12":"2019-12-20"]
-data_set.set_index('∆T', inplace=True)
-w = 432
-train_constructor = WindowSlider(window_size=w)
-data_set = df3["2019-12-12"].copy(deep=True)
-data_set.set_index('∆T', inplace=True)
-train_windows = train_constructor.collect_windows(data_set.iloc[:,1:], window_size=w, offset=w, previous_y=False)
+data_set = df3["2018-12-12":"2018-12-20"]
+data_set.drop(columns=['TIMESTAMP', 'minute_of_day', 'day', 'month', 'year'], axis=1, inplace=True)
 
-# train_windows = train_constructor.collect_windows(trainset.iloc[:, 1:],
-#                                                   previous_y=False)
+w = 5
+train_constructor = WindowSlider(window_size=w)
+
+
+train_windows = train_constructor.collect_windows(data_set, window_size=w, step=w)
+
 print("aaa")
 # df3.loc['2021-08-01':'2021-08-31'] To get certain rows
 
-#
-# N = 600
-#
-# t = np.arange(0, N, 1).reshape(-1,1)
-# t = np.array([t[i] + np.random.rand(1)/4 for i in range(len(t))])
-# t = np.array([t[i] - np.random.rand(1)/7 for i in range(len(t))])
-# t = np.array(np.round(t, 2))
-#
-# x1 = np.round((np.random.random(N) * 5).reshape(-1,1), 2)
-# x2 = np.round((np.random.random(N) * 5).reshape(-1,1), 2)
-# x3 = np.round((np.random.random(N) * 5).reshape(-1,1), 2)
-#
-# n = np.round((np.random.random(N) * 2).reshape(-1,1), 2)
-#
-# y = np.array([((np.log(np.abs(2 + x1[t])) - x2[t-1]**2) + 0.02*x3[t-3]*np.exp(x1[t-1])) for t in range(len(t))])
-# y = np.round(y+n, 2)
-#
-# dataset = pd.DataFrame(np.concatenate((t, x1, x2, x3, y), axis=1),
-#                        columns=['t', 'x1', 'x2', 'x3', 'y'])
-#
-# deltaT = np.array([(dataset.t[i + 1] - dataset.t[i]).components.minutes for i in range(len(dataset)-1)])
-# deltaT = np.concatenate((np.array([0]), deltaT))
-#
-# dataset.insert(1, '∆t', deltaT)
-# dataset.head(3)
 
